1c984a02a067cd61deea17b1b555225b29c0e751
Captainjack-kor <jennystr233@gmail.com>
2026-04-06 17:01:30 +0900

    test

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Captainjack's Blog",
+    pageTitle: "Captainjack",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
